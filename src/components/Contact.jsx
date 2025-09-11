234e488501e701675b2f77d8847776ce77944695
Ranjeet Kumar <ranjeetkumarroya23@gmail.com>
2025-09-11 10:45:01 +0530

    change contact post api

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
     setLoading(true);
     setAlertMessage("");
     try {
-      const response = await fetch("https://crm.leadstracker.in/api/contact", {
+      const response = await fetch("https://backend.leadstracker.in/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
